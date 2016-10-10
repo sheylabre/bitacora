4d335f0e0e3e556cd2feaf6a587db64412dbde20
sheylabre <sheylabrenaicha@hotmail.com>
2016-10-10 08:56:56 -0500

    css

---
+++ app.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function(){
+    var publicar = document.getElementById("publicar");
+    var texto = document.getElementById("texto");
+    var cita = document.getElementById("cita");
+    var enlace = document.getElementById("enlace");
+    var pastilla = document.getElementById("pastilla");
+
+    function presion(){}
+})
